21aae1fe72e4c51a9496aca0f39e7e3ba124623d
23521093 <156832544+hamstrous@users.noreply.github.com>
2025-12-06 23:35:50 +0700

    Add token

---
+++ Source/Token.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "axmol.h"
+#include "DraggableObject.h"
+
+class Token : public DraggableObject
+{
+public:
+    static Token* create(const std::string& texture);
+
+    virtual bool init(const std::string& texture);
+
+    static const ax::Vec2 TOKEN_SIZE;
+    static const int MOVE_TO_HOLDER_TAG;
+
+private:
+    ax::Sprite* _sprite = nullptr;
+};
